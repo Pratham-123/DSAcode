2e417acf9f6f82b20e69346763ffea719c3f9e98
Pratham Anand <54859695+Pratham-123@users.noreply.github.com>
2022-06-15 22:57:59 +0530

    Create BFS_Traversal.cpp

---
+++ Graphs/BFS_Traversal.cpp
@@ -0,0 +1,49 @@
+Link - https://www.codingninjas.com/codestudio/problems/bfs-in-graph_973002?topList=love-babbar-dsa-sheet-problems&leftPanelTab=0&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbar
+
+#include<bits/stdc++.h>
+
+void prepareAdjList(unordered_map<int, set<int> > &adjList, vector<pair<int, int>> &edges ){
+    for(int i=0;i<edges.size();i++){
+        int u = edges[i].first;
+        int v = edges[i].second;
+        
+        adjList[u].insert(v);
+        adjList[v].insert(u);
+    }
+}
+void bfs(unordered_map<int, set<int>> &adjList, unordered_map<int,bool> &visited,
+        vector<int> &ans, int node){
+    queue<int> q;
+    q.push(node);
+    visited[node] = 1;
+    
+    while(!q.empty()){
+        int frontNode = q.front();
+        q.pop();
+        
+        ans.push_back(frontNode);
+        //traverse all neighbours of frontNode
+        for(auto i:adjList[frontNode]){
+            if(!visited[i]){
+                q.push(i);
+                visited[i] = 1;
+            }
+        }
+    }
+}
+vector<int> BFS(int vertex, vector<pair<int, int>> edges)
+{
+    // Write your code here
+    unordered_map<int, set<int> > adjList;
+    vector<int> ans;
+    unordered_map<int,bool> visited;
+    
+    prepareAdjList(adjList, edges);
+    
+    for(int i=0;i<vertex;i++){
+        if(!visited[i]){
+            bfs(adjList, visited, ans, i);
+        }
+    }
+    return ans;
+}
