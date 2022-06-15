49f0eedaadc610f15b8f6e5638b3c432008d148a
Pratham Anand <54859695+Pratham-123@users.noreply.github.com>
2022-06-15 21:59:37 +0530

    Update Longest_String_Chain.cpp

--- Dynamic Programming/Longest_String_Chain.cpp
+++ Dynamic Programming/Longest_String_Chain.cpp
@@ -1,3 +1,5 @@
+Link - https://leetcode.com/problems/longest-string-chain/
+
 You are given an array of words where each word consists of lowercase English letters.
 
 wordA is a predecessor of wordB if and only if we can insert exactly one letter anywhere in wordA without changing the order of the other characters to make it equal to wordB.
