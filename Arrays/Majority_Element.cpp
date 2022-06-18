03cfaf8ec6a9410d69e6f3f1b7658fb1207ea630
Pratham Anand <54859695+Pratham-123@users.noreply.github.com>
2022-06-18 23:47:01 +0530

    Create Majority_Element.cpp

---
+++ Arrays/Majority_Element.cpp
@@ -0,0 +1,9 @@
+Link- https://leetcode.com/problems/majority-element/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+       sort(nums.begin(), nums.end());
+        return nums[nums.size()/2];
+    }
+};
