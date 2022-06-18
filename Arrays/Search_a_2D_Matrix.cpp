857dffbdfb66294998535a345c299d2ff55c8551
Pratham Anand <54859695+Pratham-123@users.noreply.github.com>
2022-06-18 23:14:00 +0530

    Create Search_a_2D_Matrix.cpp

---
+++ Arrays/Search_a_2D_Matrix.cpp
@@ -0,0 +1,30 @@
+Link- https://leetcode.com/problems/search-a-2d-matrix/
+
+class Solution {
+public:
+    bool binary(vector<int> &vec, int target){
+        int l=0, r= vec.size();
+        
+        while(l<=r)
+        {
+            int mid = (l+r)/2;
+            if(vec[mid] ==  target)
+                return true;
+            else if(vec[mid]<target)
+                l = mid + 1;
+            else 
+                r = mid -1;
+        }
+        return false;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+       if((matrix.size()==0) || (matrix[0].size() == 0)) return false;
+        int row = 0;
+        
+        while((row< matrix.size()) && (matrix[row].back()<target))
+            row++;
+        if(row >= matrix.size()) return false;
+        
+        return binary(matrix[row], target);
+    }
+};
