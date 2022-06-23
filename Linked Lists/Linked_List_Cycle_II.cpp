1b297641b45ce8b4e7c5066b888c94f61b61a9f8
Pratham Anand <54859695+Pratham-123@users.noreply.github.com>
2022-06-23 18:34:26 +0530

    Create Linked_List_Cycle_II.cpp

---
+++ Linked Lists/Linked_List_Cycle_II.cpp
@@ -0,0 +1,29 @@
+Link - https://leetcode.com/problems/linked-list-cycle-ii/
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head == NULL|| head->next == NULL) return NULL;
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* entry = head;
+        
+        while(fast->next != NULL && fast->next->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast -> next -> next;
+            if(slow == fast)
+            {
+                while(slow != entry)
+                {
+                    slow = slow->next;
+                    entry = entry-> next;
+                    
+                }
+                return entry;
+            }
+        }
+        return NULL;
+    }
+};
