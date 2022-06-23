35427809081bdf3e6d49040b3f87fd4accda4b25
Pratham Anand <54859695+Pratham-123@users.noreply.github.com>
2022-06-23 18:23:58 +0530

    Create Reverse_Nodes_in_k-Group.cpp

---
+++ Linked Lists/Reverse_Nodes_in_k-Group.cpp
@@ -0,0 +1,32 @@
+Link - https://leetcode.com/problems/reverse-nodes-in-k-group/
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head == NULL || k==1) return head;
+        
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+        
+        ListNode* pre = dummy, *cur = dummy, *nex= dummy;
+        int count = 0;
+        while(cur->next != NULL){
+            cur = cur->next;
+            count++;
+        }
+        while(count>= k){
+            cur = pre->next;
+            nex = cur->next;
+            
+            for(int i=1;i<k;i++){
+                cur->next = nex->next;
+                nex->next = pre->next;
+                pre->next = nex;
+                nex = cur->next;
+            }
+            pre = cur;
+            count-=k;
+        }
+        return dummy->next;
+    }
+};
