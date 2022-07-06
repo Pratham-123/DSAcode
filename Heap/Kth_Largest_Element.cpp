adc7e29cb5a3de11f57aaaa6fa1799fc88dec731
Pratham Anand <54859695+Pratham-123@users.noreply.github.com>
2022-07-07 00:13:07 +0530

    Create Kth_Largest_Element.cpp

---
+++ Heap/Kth_Largest_Element.cpp
@@ -0,0 +1,13 @@
+Link - https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq(nums.begin(), nums.end());
+        for (int i = 0; i < k - 1; i++) {
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
