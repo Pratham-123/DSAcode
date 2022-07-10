e4fcfc545ceaa1790f9cd00cda610788892ef7f9
Pratham Anand <54859695+Pratham-123@users.noreply.github.com>
2022-07-10 11:13:43 +0530

    Update Longest_common_SuperSequence.cpp

--- Dynamic Programming/Longest_common_SuperSequence.cpp
+++ Dynamic Programming/Longest_common_SuperSequence.cpp
@@ -1,6 +1,6 @@
 Link - https://www.geeksforgeeks.org/shortest-common-supersequence/
 
-
+TC- O(n^2)
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -42,3 +42,65 @@ int main()
  
     return 0;
 }
+
+
+
+
+
+
+*************************************************ANOTHER METHOD**********************************************
+
+Below is Another Method to solve the above problem. 
+A simple analysis yields below simple recursive solution.
+
+Let X[0..m - 1] and Y[0..n - 1] be two 
+strings and m and n be respective
+lengths.
+
+  if (m == 0) return n;
+  if (n == 0) return m;
+
+  // If last characters are same, then 
+  // add 1 to result and
+  // recur for X[]
+  if (X[m - 1] == Y[n - 1])
+     return 1 + SCS(X, Y, m - 1, n - 1);
+
+  // Else find shortest of following two
+  //  a) Remove last character from X and recur
+  //  b) Remove last character from Y and recur
+  else 
+    return 1 + min( SCS(X, Y, m - 1, n), SCS(X, Y, m, n - 1) );
+
+
+
+// A Naive recursive C++ program to find
+// length of the shortest supersequence
+#include <bits/stdc++.h>
+using namespace std;
+
+int superSeq(char* X, char* Y, int m, int n)
+{
+	if (!m)
+		return n;
+	if (!n)
+		return m;
+
+	if (X[m - 1] == Y[n - 1])
+		return 1 + superSeq(X, Y, m - 1, n - 1);
+
+	return 1
+		+ min(superSeq(X, Y, m - 1, n),
+				superSeq(X, Y, m, n - 1));
+}
+
+// Driver Code
+int main()
+{
+	char X[] = "AGGTAB";
+	char Y[] = "GXTXAYB";
+	cout << "Length of the shortest supersequence is "
+		<< superSeq(X, Y, strlen(X), strlen(Y));
+	return 0;
+}
+
