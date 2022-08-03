deb51daa7c59fe1ee241b4d1ff03a96a4ae216d9
Pratham Anand <54859695+Pratham-123@users.noreply.github.com>
2022-08-03 21:42:35 +0530

    Create Invert_Tree.cpp

---
+++ Binary Trees/Invert_Tree.cpp
@@ -0,0 +1,14 @@
+Link - https://leetcode.com/problems/invert-binary-tree/
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (!root) {
+            return NULL;
+        }
+        swap(root -> left, root -> right);
+        invertTree(root -> left);
+        invertTree(root -> right);
+        return root;
+    }
+};
